859382447d47b57363060fa6d1dbf3d9761766be
Esra Nur Deniz <esradeniz337@gmail.com>
2019-03-16 23:32:35 +0300

    Digital wardrobe organizer
    Digital wardrobe organizer which is an android aplication that helps to organize clothes and share with friends as a social platform.

---
+++ app/src/main/java/allinontech/vestium/backend/ShoppingItem.java
@@ -0,0 +1,24 @@
+package allinontech.vestium.backend;
+
+
+public class ShoppingItem {
+    private String link;
+    private String brand;
+    private String price;
+
+    public ShoppingItem() {
+
+    }
+
+    public void setLink( String link ) {
+        this.link = link;
+    }
+
+    public void setBrand ( String brand) {
+        this.brand = brand;
+    }
+
+    public void setPrice ( String price ) {
+        this.price = price;
+    }
+}
